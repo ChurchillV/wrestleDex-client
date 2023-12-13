07df638d1a072f175993caeb13036b895d59f1bb
ChurchillV <vincechurchillankrah@gmail.com>
2023-12-13 00:11:19 +0000

    Profile Card Styling

--- src/Components/WrestlerCard.js
+++ src/Components/WrestlerCard.js
@@ -6,10 +6,16 @@ const WrestlerCard = ({wrestler}) => {
         <img src={wrestler.wrestler_img} alt="#" class="w-full h-32 sm:h-48 object-contain bg-slate-600" />
         <div class="m-4">
             <span class="text-bold text-gray-200">{wrestler.wrestler_name}</span>
-            <div className="flex flex-row justify-between">
-                <span class="block text-gray-400 text-sm">{wrestler.promotion_name}</span>
-                <button className="bg-blue-200 text-gray-600 rounded-md">Edit</button>
-            </div>
+            <span className="flex flex-row justify-between">
+                <p class="block text-gray-400 text-sm">{wrestler.abbreviation}</p>
+                <button className="bg-blue-200 text-gray-600 rounded-md px-4 py-2 hover:bg-gray-600 hover:text-blue-200 transition ease-out duration-300">
+                    Edit
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+</svg>
+
+                </button>
+            </span>
         </div>
         {
             wrestler.wrestler_allegiance === 'F' ?
