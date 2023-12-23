bb511d42f0521b9acd7f184c83ae6604e1b9583b
ChurchillV <vincechurchillankrah@gmail.com>
2023-12-23 15:13:34 +0000

    Indentation fix - WrestlerCard Component

--- src/Components/WrestlerCard.js
+++ src/Components/WrestlerCard.js
@@ -23,33 +23,33 @@ const WrestlerCard = ({wrestler, onEditButtonClick}) => {
                     </button>
                 </span>
             </div>
-                {
-                    wrestler.wrestler_allegiance === 'F' ?
-                        <div class="face-badge">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
-                            </svg>
-                            <span>{wrestler.allegiance_name}</span>
-                        </div>
-                        :
-                        <div class="heel-badge">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" style={{fill : '#eeeeee'}} class="w-5 h-5 inline mr-2">
-                                <title>Evil Smile Square Emoticon Face SVG icon</title>
-                                <g><g><path d="M467.8,616.3c-84.7,0-158.3-9.9-219.1-23.4c-106.6-23.6-187.2-60-238.7-88.4c22.5,82.1,65.8,159.5,130.1,223.9c197.9,197.9,519.9,197.8,717.8,0C924.1,662.2,968.1,582,990,497.3C789.2,587.1,612.3,616.3,467.8,616.3z M742,741.7c-133.8,98.8-351.5,98.8-485.3,0c-43.5-32.2-72.8-70.9-88-111.9c34.8,14.2,89.3,32.4,161.4,44.1c41.1,6.7,90.9,11.7,148.1,11.7c97.7,0,217.3-14.6,353.1-59.5C816.5,668.5,786.8,708.6,742,741.7z"></path><path d="M674.5,361.5c38.7,0,70.1-31.4,70.1-70.2c0-19-7.6-36.2-19.8-48.8l58.6-58.6c13.9-13.9,13.9-36.4,0-50.3c-13.9-13.9-36.5-13.9-50.3,0l-107,106.9c-0.2,0.2-0.3,0.4-0.4,0.5c-13.1,12.7-21.3,30.6-21.3,50.3C604.4,330.1,635.8,361.5,674.5,361.5z"></path><path d="M284.5,242.5c-12.3,12.6-19.8,29.8-19.8,48.8c0,38.8,31.4,70.2,70.1,70.2c38.7,0,70.1-31.4,70.1-70.2c0-19.7-8.2-37.5-21.3-50.3c-0.2-0.2-0.3-0.4-0.4-0.5L276.3,133.6c-13.9-13.9-36.5-13.9-50.3,0c-13.9,13.9-13.9,36.4,0,50.3L284.5,242.5z"></path>
-                                </g></g>
-                            </svg>
-                            <span>{wrestler.allegiance_name}</span>
-                        </div>
-                }
-
-                <div className="delete-btn">
-                    <button className='text-xs tracking-wider cursor-pointer font-bold uppercase bg-red-600 text-gray-200 rounded-md px-4 py-2 hover:bg-red-400 hover:text-gray-300 transition ease-out duration-300'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            {
+                wrestler.wrestler_allegiance === 'F' ?
+                    <div class="face-badge">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
                         </svg>
-                    </button>
-                </div>
+                        <span>{wrestler.allegiance_name}</span>
                     </div>
+                    :
+                    <div class="heel-badge">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" style={{fill : '#eeeeee'}} class="w-5 h-5 inline mr-2">
+                            <title>Evil Smile Square Emoticon Face SVG icon</title>
+                            <g><g><path d="M467.8,616.3c-84.7,0-158.3-9.9-219.1-23.4c-106.6-23.6-187.2-60-238.7-88.4c22.5,82.1,65.8,159.5,130.1,223.9c197.9,197.9,519.9,197.8,717.8,0C924.1,662.2,968.1,582,990,497.3C789.2,587.1,612.3,616.3,467.8,616.3z M742,741.7c-133.8,98.8-351.5,98.8-485.3,0c-43.5-32.2-72.8-70.9-88-111.9c34.8,14.2,89.3,32.4,161.4,44.1c41.1,6.7,90.9,11.7,148.1,11.7c97.7,0,217.3-14.6,353.1-59.5C816.5,668.5,786.8,708.6,742,741.7z"></path><path d="M674.5,361.5c38.7,0,70.1-31.4,70.1-70.2c0-19-7.6-36.2-19.8-48.8l58.6-58.6c13.9-13.9,13.9-36.4,0-50.3c-13.9-13.9-36.5-13.9-50.3,0l-107,106.9c-0.2,0.2-0.3,0.4-0.4,0.5c-13.1,12.7-21.3,30.6-21.3,50.3C604.4,330.1,635.8,361.5,674.5,361.5z"></path><path d="M284.5,242.5c-12.3,12.6-19.8,29.8-19.8,48.8c0,38.8,31.4,70.2,70.1,70.2c38.7,0,70.1-31.4,70.1-70.2c0-19.7-8.2-37.5-21.3-50.3c-0.2-0.2-0.3-0.4-0.4-0.5L276.3,133.6c-13.9-13.9-36.5-13.9-50.3,0c-13.9,13.9-13.9,36.4,0,50.3L284.5,242.5z"></path>
+                            </g></g>
+                        </svg>
+                        <span>{wrestler.allegiance_name}</span>
+                    </div>
+            }
+
+            <div className="delete-btn">
+                <button className='text-xs tracking-wider cursor-pointer font-bold uppercase bg-red-600 text-gray-200 rounded-md px-4 py-2 hover:bg-red-400 hover:text-gray-300 transition ease-out duration-300'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                </button>
+            </div>
+        </div>
     )
 }
 
